17e5c4421e8a3fe1d2c418a955478d08fe81f945
hschilbert <41989160+hschilbert@users.noreply.github.com>
2021-06-23 12:38:29 +0200

    Update coexp_bn.py

--- coexp_bn.py
+++ coexp_bn.py
@@ -1,6 +1,6 @@
 
 __usage__ = """
-					python coexp.py
+					python coexp_bn.py
 					--in <FULL_PATH_TO_CANDIDATE_FILE(one ID per line)>
 					--exp <FULL_PATH_TO_EXPRESSION_TABLE>
 					--ann <FULL_PATH_TO_ANNOTATION_FILE>
